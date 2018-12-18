4a5f71cf70897e7b13a167b9a58b232240066469
hs <hs@hsdeMacBook-Pro.local>
2018-12-18 21:56:36 +0800

    mac system was crashed by win10 update.A big shit update.Reinstall mac and restore some software and settings. Rewrite zombieKonga working which has been done onMonday.

--- zombieKonga/GameScene.swift
+++ zombieKonga/GameScene.swift
@@ -7,83 +7,169 @@
 //
 
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene
+{
+    var dt: TimeInterval = 0
+    var zombieMovePointsPerSec: CGFloat = 480.0
+    var velocity = CGPoint(x: 0, y: 0)
+    let playableRect: CGRect
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
-    override func didMove(to view: SKView) {
+    let cat1 = SKSpriteNode(imageNamed: "cat")
+    let cat2 = SKSpriteNode(imageNamed: "cat")
+    let zombie = SKSpriteNode(imageNamed: "zombie1")
+    
+    override func didMove(to view: SKView)
+    {
+        backgroundColor = SKColor.white
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        background.zPosition = -1
+        addChild(background)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        cat1.position = CGPoint(x: size.width / 8, y: size.height / 2)
+        cat1.zPosition = 0
+        addChild(cat1)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        cat2.position = CGPoint(x: size.width / 8 * 3, y: size.height / 2)
+        cat2.zPosition = 0
+        addChild(cat2)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        zombie.position = CGPoint(x: size.width / 8, y: size.height / 4)
+        zombie.zPosition = 0
+        addChild(zombie)
+        
+        debugDrawPlayableArea()
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    override init(size: CGSize)
+    {
+        let maxAspectRatio:CGFloat = 16.0/9.0
+        // 1
+        let playableHeight = size.width / maxAspectRatio
+        // 2
+        let playableMargin = (size.height-playableHeight)/2.0
+        // 3
+        playableRect = CGRect(x: 0, y: playableMargin,width: size.width,height: playableHeight)
+        // 4
+        super.init(size: size)
+        // 5
+    }
+    required init(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+        // 6
+    }
+    func moveSprite(sprite: SKSpriteNode, velocity: CGPoint)
+    {
+        // 1
+        let amountToMove = CGPoint(x: velocity.x * CGFloat(dt),
+                                   y: velocity.y * CGFloat(dt))
+//        print("Amount to move: \(amountToMove)")
+        // 2
+        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x,
+                                  y: sprite.position.y + amountToMove.y)
+        
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func moveZombieToward(sprite: SKSpriteNode, location: CGPoint)
+    {
+        let offset = CGPoint(x: location.x - sprite.position.x,
+                             y: location.y - sprite.position.y)
+        let length = sqrt(Double(offset.x * offset.x + offset.y * offset.y))
+        let direction = CGPoint(x: offset.x / CGFloat(length), y: offset.y / CGFloat(length))
+        velocity = CGPoint(x: direction.x * zombieMovePointsPerSec, y: direction.y * zombieMovePointsPerSec)
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func boundsCheckZombie()
+    {
+//        let bottomLeft = CGPoint(x: 0, y: 0)
+//        let topRight = CGPoint(x: size.width, y: size.height)
+        let bottomLeft = CGPoint(x: 0,
+                                 y: playableRect.minY)
+        let topRight = CGPoint(x: size.width,
+                               y: playableRect.maxY)
+        if zombie.position.x <= bottomLeft.x
+        {
+            zombie.position.x = bottomLeft.x
+            velocity.x = -velocity.x
+        }
+        if zombie.position.x >= topRight.x
+        {
+            zombie.position.x = topRight.x
+            velocity.x = -velocity.x
+        }
+        if zombie.position.y <= bottomLeft.y
+        {
+            zombie.position.y = bottomLeft.y
+            velocity.y = -velocity.y
+        }
+        if zombie.position.y >= topRight.y
+        {
+            zombie.position.y = topRight.y
+            velocity.y = -velocity.y
+        }
     }
     
+    func debugDrawPlayableArea()
+    {
+        let shape = SKShapeNode()
+        let path = CGMutablePath.init()
+        path.addRect(playableRect)
+        shape.path = path
+        shape.strokeColor = SKColor.red
+        shape.lineWidth = 4.0
+        addChild(shape)
+    }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func rotateSprite(sprite: SKSpriteNode, direction: CGPoint)
+    {
+        sprite.zRotation = CGFloat(atan2(Double(direction.y),
+                                         Double(direction.x)))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        //        if let label = self.label {
+        //            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        //        }
+        
+        //        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        let touch = touches.first
+        if(touch != nil)
+        {
+            moveZombieToward(sprite: zombie, location: touch!.location(in: self))
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        //for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        let touch = touches.first
+        if(touch != nil)
+        {
+            moveZombieToward(sprite: zombie, location: touch!.location(in: self))
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        //for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        //for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func update(_ currentTime: TimeInterval)
+    {
+        dt = 1 / 60
+        //moveSprite(sprite: zombie, velocity: CGPoint(x: zombieMovePointsPerSec, y: 0))
+        moveSprite(sprite: zombie, velocity: velocity)
+        rotateSprite(sprite: zombie, direction: velocity)
+        
     }
 }
--- zombieKonga/GameViewController.swift
+++ zombieKonga/GameViewController.swift
@@ -11,44 +11,29 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
+        if let skView = self.view as! SKView?
+        {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+            scene.scaleMode = .aspectFill
             
-            view.ignoresSiblingOrder = true
+            skView.ignoresSiblingOrder = true
+            skView.showsFPS = true
+            skView.showsNodeCount = true
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            skView.presentScene(scene)
         }
+        
     }
-
+    
     override var shouldAutorotate: Bool {
-        return true
+        return false
     }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
