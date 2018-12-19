8ab8421b838916295b32702fd885435f0a095c50
hs <hs@hsdeMacBook-Pro.local>
2018-12-19 14:09:45 +0800

    1.modefied the rotation to make it smooth 2.added action for cat 3.added collision using a simpling method ,not the box2d system 4.added music

--- zombieKonga/GameScene.swift
+++ zombieKonga/GameScene.swift
@@ -12,14 +12,14 @@ class GameScene: SKScene
 {
     var dt: TimeInterval = 0
     var zombieMovePointsPerSec: CGFloat = 480.0
+    var zombieRotateRadiansPerSec:CGFloat = 4.0 * π
     var lastTouchPosition = CGPoint(x: 0, y: 0)
     var velocity = CGPoint(x: 0, y: 0)
     
     let playableRect: CGRect
-    let cat1 = SKSpriteNode(imageNamed: "cat")
-    let cat2 = SKSpriteNode(imageNamed: "cat")
     let zombie = SKSpriteNode(imageNamed: "zombie1")
-    
+    let zombieAnimation: SKAction
+    let backgroundMusicAction : SKAction
     override func didMove(to view: SKView)
     {
         backgroundColor = SKColor.white
@@ -29,19 +29,16 @@ class GameScene: SKScene
         background.zPosition = -1
         addChild(background)
         
-        cat1.position = CGPoint(x: size.width / 8, y: size.height / 2)
-        cat1.zPosition = 0
-        addChild(cat1)
-        
-        cat2.position = CGPoint(x: size.width / 8 * 3, y: size.height / 2)
-        cat2.zPosition = 0
-        addChild(cat2)
-        
         zombie.position = CGPoint(x: size.width / 8, y: size.height / 4)
         zombie.zPosition = 0
         addChild(zombie)
         
         debugDrawPlayableArea()
+        run(SKAction.repeatForever(SKAction.sequence([SKAction.run(spawnEnemy),
+                                                      SKAction.wait(forDuration: Double(0.5 + CGFloat.random()))])))
+        run(SKAction.repeatForever(SKAction.sequence([SKAction.run(spawnCat),
+                                                      SKAction.wait(forDuration: Double(0.5 + CGFloat.random()))])))
+        run(SKAction.repeatForever(backgroundMusicAction))
     }
     
     override init(size: CGSize)
@@ -54,14 +51,28 @@ class GameScene: SKScene
         // 3
         playableRect = CGRect(x: 0, y: playableMargin,width: size.width,height: playableHeight)
         // 4
+        var textures : [SKTexture] = []
+//        for i in 1...4 {
+//            textures.append(SKTexture(imageNamed: "zombie\(i)"))
+//        }
+        textures.append(SKTexture(imageNamed: "zombie1"))
+        textures.append(SKTexture(imageNamed: "zombie2"))
+        textures.append(SKTexture(imageNamed: "zombie3"))
+        textures.append(SKTexture(imageNamed: "zombie4"))
+        textures.append(textures[2])
+        textures.append(textures[1])
+        zombieAnimation = SKAction.repeatForever(SKAction.animate(with: textures, timePerFrame: 0.1))
+//        zombieAnimation = SKAction.repeatForever(SKAction.animate(withNormalTextures: textures, timePerFrame: 0.1))//This mothed has no effect,use method above
+        backgroundMusicAction = SKAction.playSoundFileNamed("backgroundMusic.mp3", waitForCompletion: true)
         super.init(size: size)
-        // 5
+
     }
     required init(coder aDecoder: NSCoder)
     {
         fatalError("init(coder:) has not been implemented")
         // 6
     }
+    
     func moveSprite(sprite: SKSpriteNode, velocity: CGPoint)
     {
 //        // 1
@@ -82,6 +93,7 @@ class GameScene: SKScene
 //        let length = sqrt(Double(offset.x * offset.x + offset.y * offset.y))
 //        let direction = CGPoint(x: offset.x / CGFloat(length), y: offset.y / CGFloat(length))
 //        velocity = CGPoint(x: direction.x * zombieMovePointsPerSec, y: direction.y * zombieMovePointsPerSec)
+        startZombieAnimation()
         let offset = location - sprite.position
         velocity = offset.normalized() * zombieMovePointsPerSec
     }
@@ -129,11 +141,24 @@ class GameScene: SKScene
         addChild(shape)
     }
     
-    func rotateSprite(sprite: SKSpriteNode, direction: CGPoint)
+    func rotateSprite(sprite: SKSpriteNode, direction: CGPoint, rotateRadiansPerSec: CGFloat)
     {
-        sprite.zRotation = CGFloat(atan2(Double(direction.y),
-                                         Double(direction.x)))
+        let angleBet = shortestAngleBetween(angle1: sprite.zRotation,angle2: direction.angle)
+        if angleBet >= CGFloat(dt) * rotateRadiansPerSec * 0.5
+        {
+            sprite.zRotation += CGFloat(dt) * rotateRadiansPerSec
+        }
+        else if angleBet < -1 * CGFloat(dt) * rotateRadiansPerSec * 0.5
+        {
+            sprite.zRotation -= CGFloat(dt) * rotateRadiansPerSec
+        }
     }
+    
+//    func rotateSprite(sprite: SKSpriteNode, direction: CGPoint)
+//    {
+//        sprite.zRotation = CGFloat(atan2(Double(direction.y),
+//                                         Double(direction.x)))
+//    }
     
     func calDistanceBetTouchAndSprite(sprite: SKSpriteNode) -> Bool
     {
@@ -146,6 +171,117 @@ class GameScene: SKScene
         else
         {
             return false
+        }
+    }
+    
+    func spawnEnemy()
+    {
+        let enemy = SKSpriteNode(imageNamed: "enemy")
+        enemy.name = "enemy"
+        enemy.position = CGPoint(x: size.width + enemy.size.width/2, y: CGFloat.random(min: playableRect.minY + enemy.size.height / 2,
+                                                                                       max: playableRect.maxY - enemy.size.height / 2))
+        addChild(enemy)
+        
+        let actionMove = SKAction.move(to: CGPoint(x: -enemy.size.width/2, y: enemy.position.y), duration: Double(0.5 + CGFloat.random()))
+//        enemy.run(actionMove)
+        
+        // 1
+//        let actionMidMove = SKAction.move(to: CGPoint(x: size.width/2,
+//                                                      y: (playableRect.minY) + enemy.size.height/2) , duration: Double(CGFloat.random()))
+        // 2
+//        let actionMove = SKAction.move(to:CGPoint(x: -enemy.size.width/2,
+//                                                  y: enemy.position.y), duration:Double(CGFloat.random()))
+//        let wait = SKAction.wait(forDuration: 0.5)
+//        let logMessage = SKAction.run({
+//            print("Reached bottom!")
+//        })
+//        let reverseMid = actionMidMove.reversed()
+//        let reverseMove = actionMove.reversed()
+        // 3
+//        let halfSequence = SKAction.sequence([actionMidMove, logMessage, wait, actionMove])
+//        let sequence = SKAction.sequence([halfSequence, halfSequence.reversed()])
+        let removeAction = SKAction.removeFromParent()
+        let sequence = SKAction.sequence([actionMove, removeAction])
+        
+        // 4
+        enemy.run(sequence)
+    }
+    
+    func spawnCat(){
+        // 1
+        let cat = SKSpriteNode(imageNamed: "cat")
+        cat.name = "cat"
+        cat.position = CGPoint(x: CGFloat.random(min: playableRect.minX, max: playableRect.maxX),
+                               y: CGFloat.random(min: playableRect.minY, max: playableRect.maxY))
+        cat.setScale(0)
+        addChild(cat)
+        // 2
+        let appear = SKAction.scale(to: 1.0, duration: 0.5)
+        cat.zRotation = -π / 16.0
+        let leftWiggle = SKAction.rotate(byAngle: π/8.0, duration: 0.5)
+        let rightWiggle = leftWiggle.reversed()
+        let fullWiggle = SKAction.sequence([leftWiggle, rightWiggle])
+        
+        let scaleUp = SKAction.scale(by:1.2, duration: 0.25)
+        let scaleDown = scaleUp.reversed()
+        let fullScale = SKAction.sequence([scaleUp, scaleDown, scaleUp, scaleDown])
+        let group = SKAction.group([fullScale, fullWiggle])
+        let groupWait = SKAction.repeat(group, count: 10)
+        
+        let disappear = SKAction.scale(to: 0, duration: 0.5)
+        let removeFromParent = SKAction.removeFromParent()
+        let actions = [appear, groupWait, disappear, removeFromParent]
+        cat.run(SKAction.sequence(actions))
+    }
+    
+    func startZombieAnimation()
+    {
+        if zombie.action(forKey: "animation") == nil {
+            zombie.run(SKAction.repeatForever(zombieAnimation), withKey: "animation")
+        }
+    }
+    func stopZombieAnimation(){
+        zombie.removeAction(forKey: "animation")
+    }
+    
+    func zombieHitCat(cat: SKSpriteNode){
+        cat.removeFromParent()
+        run(SKAction.playSoundFileNamed("hitCat.wav", waitForCompletion: false))
+    }
+    
+    func zombieHitEnemy(enemy: SKSpriteNode){
+        zombie.removeFromParent()
+        run(SKAction.playSoundFileNamed("hitCatLady.wav", waitForCompletion: false))
+    }
+    
+    func collisionDetect(){
+        var hitCats : [SKSpriteNode] = []
+        enumerateChildNodes(withName: "cat", using: { node, _ in
+            let cat = node as? SKSpriteNode
+            if cat != nil
+            {
+                if cat!.frame.intersects(self.zombie.frame.insetBy(dx: 50, dy: 50)){
+                    hitCats.append(cat!)
+                }
+            }
+        })
+        for cat in hitCats{
+            zombieHitCat(cat: cat)
+        }
+        
+        var hitEnemies : [SKSpriteNode] = []
+        enumerateChildNodes(withName: "enemy", using: { node, _ in
+            let enemy = node as? SKSpriteNode
+            if enemy != nil{
+                //zombieRect:(99.0, 282.0, 314.0, 204.0)
+                //enemyRect:(1726.04895019531, 237.656005859375, 232.0, 322.0)
+                if enemy!.frame.insetBy(dx: 50, dy: 50).intersects(self.zombie.frame.insetBy(dx: 50, dy: 50)){
+                    hitEnemies.append(enemy!)
+                }
+            }
+        })
+        for enemy in hitEnemies{
+            zombieHitEnemy(enemy: enemy)
         }
     }
     
@@ -196,7 +332,19 @@ class GameScene: SKScene
         {
             moveSprite(sprite: zombie, velocity: velocity)
         }
-        rotateSprite(sprite: zombie, direction: velocity)
+        else
+        {
+            stopZombieAnimation()
+        }
+        //rotateSprite(sprite: zombie, direction: velocity)
+        rotateSprite(sprite: zombie, direction: velocity,rotateRadiansPerSec: zombieRotateRadiansPerSec)
         boundsCheckZombie(sprite: zombie)
+//        collisionDetect()
     }
+    
+    override func didEvaluateActions(){
+        collisionDetect()
+    }
+    
 }
+
